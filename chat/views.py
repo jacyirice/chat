e772f7e104c8993f8ef0c7d27f48ed1da75a017c
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-05-12 03:16:00 -0300

    Adicionando decorators

--- chat/views.py
+++ chat/views.py
@@ -1,21 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Group
 
 # Create your views here.
 
-
+@login_required
 def index(request):
-    if request.user.is_authenticated:
-        groups = request.user.my_groups.all()
-        return render(request, 'chat/index.html', {'groups':groups})
-    return redirect('/account/login')
+    groups = request.user.my_groups.all()
+    return render(request, 'chat/index.html', {'groups':groups})
 
-
+@login_required
 def room(request, room_name):
-    if request.user.is_authenticated:
-        group = get_object_or_404(Group, pk=room_name)
-        return render(request, 'chat/room.html', {
-            'group': group,
-            'room_name': group.pk
-        })
-    return redirect('/account/login')
+    group = get_object_or_404(Group, pk=room_name)
+    return render(request, 'chat/room.html', {
+        'group': group,
+        'room_name': group.pk
+    })
